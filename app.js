83908b478db25d65802c77d68e5c01ab85863272
Nathan Szelag <nszelag@live.com>
2019-05-23 04:09:19 -0400

    added 404 response to invalid search and error handling

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ function fetchImages() {
         displayResults(data);
         }
     )
-    .catch((err) => console.error('Something Went Wrong!')) 
+    .catch((err) => console.error('Something Went Wrong!', err)) 
 }
 
 function displayResults(data) {
@@ -24,12 +24,32 @@ function numberForm() {
     });
 }
 
-function breedForm() {
+function fetchBreedImage() {
+    const breedSearchValue = $('#breed').val();
+    fetch(`https://dog.ceo/api/breed/${breedSearchValue}/images/random`)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        if (data.code === '404') {
+            $('#breedPhoto').html(`<h3>Your search was Invalid. Try Again!</h3><img src="images/pac-404.png">`)
+        } else {
+            $('#breedPhoto').html(`<h3>Your result for ${breedSearchValue}:</h3><br><img src=${data.message}>`)
+        }
+    })
+    .catch(err => console.log('Something didn\'t go right.', err))
+}
 
+function breedForm() {
+    $('#breedSearch').on('submit', function(e){
+        e.preventDefault();
+        fetchBreedImage();
+        resetPage();
+    })
 }
 
 function resetPage() {
-    $('#dogPhotos').html('')
+    $('#dogPhotos').html('');
+    $('#breedPhoto').html('');
 }
 
 
